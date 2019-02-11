cb0c64f4094e6d7e1656a62a2f14ea85c6f33fc9
parvizrovshanaliyev <parvizra@code.edu.az>
2019-02-11 08:53:00 +0400

    heleki dizayn

--- CompBase_test1/CompBase_test1/MainWindow.xaml.cs
+++ CompBase_test1/CompBase_test1/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CompBase_test1.classes;
+using CompBase_test1.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,16 +23,14 @@ namespace CompBase_test1
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool StateClosed { get; private set; }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
+        
         private void UpBorder_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (Mouse.LeftButton == MouseButtonState.Pressed)
@@ -40,7 +41,40 @@ namespace CompBase_test1
 
         private void HomeBtn_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("salam");
+            UserControl_Call.UserControl_Add(Contents, new Home());
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            UserControl_Call.UserControl_Add(Contents, new Home());
+        }
+
+        private void Purchase_Click(object sender, RoutedEventArgs e)
+        {
+            UserControl_Call.UserControl_Add(Contents, new Purchase());
+        }
+
+
+
+
+
+        #region Window Control buttons
+
+        private void BtnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void BtnMaxmize_Click(object sender, RoutedEventArgs e)
+        {
+            //this.WindowState = WindowState.Maximized;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        
     }
 }
---
+++ CompBase_test1/CompBase_test1/classes/UserControl_Call.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CompBase_test1.classes
+{
+    public class UserControl_Call
+    {
+        public static void UserControl_Add(Grid grid, UserControl userControl)
+        {
+            if (grid.Children.Count > 0)
+            {
+                grid.Children.Clear();
+                grid.Children.Add(userControl);
+            }
+            else
+            {
+                grid.Children.Add(userControl);
+
+            }
+        }
+    }
+}
